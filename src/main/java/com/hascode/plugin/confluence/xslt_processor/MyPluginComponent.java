43bdfe1c80d64131c57b58ba58c7962e0395ffb4
Micha Kops <micha.kops@gmail.com>
2013-05-05 20:54:21 +0200

    Configuration action setup.

--- src/main/java/com/hascode/plugin/confluence/xslt_processor/MyPluginComponent.java
+++
@@ -1,6 +0,0 @@
-package com.hascode.plugin.confluence.xslt_processor;
-
-public interface MyPluginComponent
-{
-    String getName();
-}
---
+++ src/main/java/com/hascode/plugin/confluence/xslt_processor/action/ConfigureAction.java
@@ -0,0 +1,12 @@
+package com.hascode.plugin.confluence.xslt_processor.action;
+
+import com.atlassian.confluence.core.ConfluenceActionSupport;
+
+public class ConfigureAction extends ConfluenceActionSupport {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String execute() {
+		return SUCCESS;
+	}
+}
