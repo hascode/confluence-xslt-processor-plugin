4d3f3d845a028b79c9d813c808b7aa096b2792ba
Micha Kops <micha.kops@gmail.com>
2013-05-20 16:01:12 +0200

    Issue #3 fixed: Permission checks for the processing action implemented.

--- src/main/java/com/hascode/plugin/confluence/xslt_processor/servlet/XsltTransformerServlet.java
+++ src/main/java/com/hascode/plugin/confluence/xslt_processor/servlet/XsltTransformerServlet.java
@@ -7,11 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.atlassian.confluence.core.ContentEntityObject;
 import com.atlassian.confluence.pages.PageManager;
+import com.atlassian.confluence.security.Permission;
+import com.atlassian.confluence.security.PermissionManager;
+import com.atlassian.confluence.user.UserAccessor;
+import com.atlassian.user.User;
 import com.google.common.base.Optional;
 import com.hascode.plugin.confluence.xslt_processor.entity.XsltTemplate;
 import com.hascode.plugin.confluence.xslt_processor.repository.XsltTemplateProvider;
@@ -24,12 +29,18 @@ public class XsltTransformerServlet extends HttpServlet {
 	private static final XsltPageTransformer transformer = new XsltPageTransformer();
 	private final XsltTemplateProvider xsltTemplateProvider;
 	private final PageManager pageManager;
+	private final PermissionManager permissionManager;
+	private final UserAccessor userAccessor;
 
 	public XsltTransformerServlet(
 			final XsltTemplateProvider xsltTemplateProvider,
-			final PageManager pageManager) {
+			final PageManager pageManager,
+			final PermissionManager permissionManager,
+			final UserAccessor userAccessor) {
 		this.xsltTemplateProvider = xsltTemplateProvider;
 		this.pageManager = pageManager;
+		this.permissionManager = permissionManager;
+		this.userAccessor = userAccessor;
 	}
 
 	@Override
@@ -48,19 +59,50 @@ public class XsltTransformerServlet extends HttpServlet {
 			return;
 		}
 
-		Optional<XsltTemplate> template = findTemplate(ctx);
-		if (!template.isPresent()) {
-			printErrorMessage(ctx, res);
-			return;
-		}
-
 		ContentEntityObject ceo = pageManager.getById(ctx.getPageId().get());
 		if (ceo == null) {
 			printErrorMessage(ctx, res);
 			return;
 		}
 
+		if (!userHasViewPermissions(req, ceo)) {
+			printPermissionErrorMessage(res);
+			return;
+		}
+
+		Optional<XsltTemplate> template = findTemplate(ctx);
+		if (!template.isPresent()) {
+			printErrorMessage(ctx, res);
+			return;
+		}
+
 		printTransformationResult(res, template, ceo);
+	}
+
+	private void printPermissionErrorMessage(final HttpServletResponse res) {
+		try {
+			res.getWriter().append(
+					"Sorry, insufficient permission to view this page");
+		} catch (IOException e) {
+			log.warn("io exception thrown", e);
+		}
+	}
+
+	private boolean userHasViewPermissions(final HttpServletRequest req,
+			final ContentEntityObject ceo) {
+		String userName = req.getRemoteUser();
+		if (StringUtils.isEmpty(userName)) {
+			log.warn("no user name found in request .. skipping output");
+			return false;
+		}
+
+		User user = userAccessor.getUser(userName);
+		if (user == null) {
+			log.warn("no user object for given name {} found", userName);
+			return false;
+		}
+
+		return permissionManager.hasPermission(user, Permission.VIEW, ceo);
 	}
 
 	private void printTransformationResult(final HttpServletResponse res,
